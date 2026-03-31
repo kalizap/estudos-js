891bebc76188d01c8cf11e3f5458c8b81a00da2d
kalinca-paz <kalincatrabalho@gmail.com>
2026-03-31 18:16:28 -0300

    Meu primeiro commit de JS

---
+++ tipos de saida/js/script.js
@@ -0,0 +1,7 @@
+//TIPOS DE SAIDAS 30/03/2026 16:16
+
+// para alterar diretamente o html
+document.getElementById("text").innerHTML = "Meu texto <br>Javascript</br>";
+// aparece só no terminal
+console.log('Olá, mundo!'); 
+alert('Alerta externo');
