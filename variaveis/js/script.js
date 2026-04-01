5e433593bda8ce0ffd6db81fc90a03769674827f
kalinca-paz <kalincatrabalho@gmail.com>
2026-04-01 15:12:35 -0300

    2 dias de estudo

---
+++ variaveis/js/script.js
@@ -0,0 +1,20 @@
+/* TIPOS DE VARIAVEIS 30/03/2026 19:15
+->var
+->let
+->const
+*/
+
+/*VAR
+- pode retribuição,
+-
+
+
+
+var a = 2;
+var b = 3;
+var c = a + b; 
+
+alert(c);
+*/
+//let
+console.log(document.querySelectorAll("script"));
