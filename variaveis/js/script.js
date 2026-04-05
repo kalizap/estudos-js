e8dbc01ce4bd7988b611d995f6bc3cbc61e394c6
kalinca-paz <zzapkali@gmail.com>
2026-04-05 16:50:30 -0300

    Adicionando layout responsivo da faculdade

--- variaveis/js/script.js
+++ variaveis/js/script.js
@@ -5,16 +5,39 @@
 */
 
 /*VAR
-- pode retribuição,
--
 
+//var a = 2;
+//var b = 3;
+//var c = a + b; 
 
+MESMA COISA */
+var a,b,c;
+a =2;
+b=3;
+c=a+b;
+//alert(c);
 
-var a = 2;
-var b = 3;
-var c = a + b; 
+//Pertmite reatribuição==
+c = 9;
+//alert(c)
 
-alert(c);
+var nome, sobrenome, nomeClmpleto;
+nome = "Kalinca";
+sobrenome = "Souza";
+nomeCompleto = nome + " "+ sobrenome;
+document.getElementById("nome").innerHTML = nomeCompleto;
+
+/*LET
+Não permite reatribuição(somente no escopo)
+
 */
-//let
-console.log(document.querySelectorAll("script"));
+let n1, n2, n3;//mais usadoo
+n1 = 5; 
+n2 = 3;
+n3 = n1 + n2;
+document.getElementById("valor").innerHTML = n3;
+/*
+CONST
+-Não permite reatribuição
+-nunca muda
+*/
